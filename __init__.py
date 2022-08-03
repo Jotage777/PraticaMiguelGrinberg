e367eb0a7cd27a29c88b7261437980234209199c
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-08-03 14:53:41 -0300

    Chapter 3 - ok

--- __init__.py
+++ __init__.py
@@ -1,25 +1,31 @@
-from flask import Flask, request,render_template
-from flask_restful import Resource, Api, abort
-
+from flask import Flask, render_template,redirect,flash,url_for
+from config import Config
+from forms import LoginForm
 app = Flask(__name__)
-api = Api(app)
+app.config.from_object(Config)
 
-class Index(Resource):
-    def get(self):
-        user={'username': 'Gabriel'}
-        posts=[
-            {
-                'author': {'username':'John'},
-                'body': 'O dia amanheceu chuvoso em Campina Grande'
-            },
-            {
-                'author': {'username':'Daniel'},
-                'body':"Belo dia para programar"
-            }
-        ]
-        return render_template('index.html',title = 'Home',user=user,posts=posts)
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Gabriel'}
+    posts = [
+        {
+            'author': {'username': 'Gabriel'},
+            'body': 'Amanheceu um dia chuvoso em Campina Grande!'
+        },
+        {
+            'author': {'username': 'Daniel'},
+            'body': 'Acordei disposto a trabalhar!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-
-api.add_resource(Index,"/index")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 if __name__ == "__main__":
     app.run(debug=True)
