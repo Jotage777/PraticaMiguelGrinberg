66695049e5f33096c748f524be5bcf1fae14d5c3
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-08-03 16:49:09 -0300

    Chapter 5 - ok

--- __init__.py
+++ __init__.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template,redirect,flash,url_for
+from flask import Flask, render_template,redirect,flash,url_for,request
 from config import Config
 from forms import LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.urls import url_parse
+from flask_login import LoginManager,UserMixin,current_user, login_user,logout_user,login_required
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,9 +35,12 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Gabriel'}
     posts = [
@@ -41,14 +53,28 @@ def index():
             'body': 'Acordei disposto a trabalhar!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
