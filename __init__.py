3ddc418cb9b8f9441c0cece2229f33834bdacf57
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-08-03 17:01:50 -0300

    Chapter 6 - ok

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template,redirect,flash,url_for,request
 from config import Config
-from forms import LoginForm
+from forms import LoginForm, EditProfileForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from flask_login import LoginManager,UserMixin,current_user, login_user,logout_user,login_required
+from hashlib import md5
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -27,6 +28,14 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,6 +65,7 @@ def index():
     return render_template("index.html", title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@login_required
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -72,9 +82,41 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
