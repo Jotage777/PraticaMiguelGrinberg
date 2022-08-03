dbe869b1b7bc5f23a60926b89c4b2ddda47d6ea4
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-08-03 16:02:38 -0300

    Chapter 4 - ok

--- __init__.py
+++ __init__.py
@@ -1,8 +1,31 @@
 from flask import Flask, render_template,redirect,flash,url_for
 from config import Config
 from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
 @app.route('/')
 @app.route('/index')
